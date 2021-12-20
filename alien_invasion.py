5ad37f09290ffe83ff508194a7c9fbaddff76db2
Yerbulan <bula136@gmail.com>
2021-12-20 13:12:16 +0600

    December 20, added bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 
 import pygame
+from pygame.sprite import Group
 import game_functions as gf
 
 from settings import Settings
@@ -15,10 +16,13 @@ def run_game():
 	pygame.display.set_caption("Alien Invasion")
 	# Make a ship.
 	ship = Ship(ai_settings, screen)
+	# Make a group to store bullets in.
+	bullets = Group()
 	# Start the main loop for the game.
 	while True:
-		gf.check_events(ship)
+		gf.check_events(ai_settings, screen, ship, bullets)
 		ship.update()
-		gf.update_screen(ai_settings, screen, ship)
+		bullets.update()
+		gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
