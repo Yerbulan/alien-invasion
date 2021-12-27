dd1c1001ea9bd4e7d6da0436c5c526f7878c322b
Yerbulan <bula136@gmail.com>
2021-12-27 20:16:18 +0600

    Dec 27 Added scoring system, needs work

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,8 +6,10 @@ from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 from alien import Alien
+from game_stats import GameStats
+from scoreboard import Scoreboard
+from button import Button
 import game_functions as gf
-
 
 #temporary code
 from star import Star
@@ -19,6 +21,11 @@ def run_game():
 	screen = pygame.display.set_mode(
 		(ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
+	# Make the Play button.
+	play_button = Button(ai_settings, screen, "Play")
+	# Create an instance to store game statistics and create a scoreboard.
+	stats = GameStats(ai_settings)
+	sb = Scoreboard(ai_settings, screen, stats)
 	# Make a ship, a group of bullets, and a group of aliens.
 	ship = Ship(ai_settings, screen)
 	# Make a group to store bullets in.
@@ -28,11 +35,14 @@ def run_game():
 	# Create the fleet of aliens.
 	gf.create_stars(ai_settings, screen, stars)
 	gf.create_fleet(ai_settings, screen, ship, aliens)
+
 	# Start the main loop for the game.
 	while True:
-		gf.check_events(ai_settings, screen, ship, bullets)
-		ship.update()
-		gf.update_bullets(bullets)
-		gf.update_screen(ai_settings, screen, ship, aliens, bullets, stars)
+		gf.check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
+		if stats.game_active:
+			ship.update()
+			gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+			gf.update_aliens(ai_settings,stats, screen, ship, aliens, bullets)
 
+		gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button, stars)
 run_game()
